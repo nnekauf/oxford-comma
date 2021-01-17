3046fddc75111d2f0d6a601a897d57bc3bb25a10
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-17 00:05:28 -0500

    commit tree 64db8cf8be0d8472be384660e07e20e516154dda

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+   
+  
+    if array.length >2   # if the array given had more than two elements,
+       array[-1]= "and " + array[-1] # transform the element at the last position into the elements at the last posisiton plus the word and infront
+      # now we fixed the last element but we need to join in the rest of the elements seperated by a comma
+       array.join(", ")
+    
+    else                            
+        array.join(" and ")
+   end
+   
+   #end
 end
